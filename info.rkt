7ca8f337a14f8edf71dd42f877e03d0a70e7d899
Neil Locketz <neil@nlocketz.com>
2018-03-23 21:36:56 -0400

    Remove unused dependency

--- info.rkt
+++ info.rkt
@@ -2,4 +2,4 @@
 (define version "0.0")
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/example.scrbl" ())))
-(define deps '("quickcheck" "functional-lib"))
+(define deps '("quickcheck"))
