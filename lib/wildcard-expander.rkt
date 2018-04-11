c90a07b2fd0c5dfad7aca15c9aa5065570fbe8fe
Neil Locketz <neil@nlocketz.com>
2018-04-11 18:36:32 -0400

    checkpoint

--- lib/wildcard-expander.rkt
+++ lib/wildcard-expander.rkt
@@ -11,13 +11,12 @@
        (eq? 1 (- (cdr x)
                  (car x)))))
 
-(define (handle-next remain-mchs remain-eles)
+(define (handle-next remain-mchs)
   (cond
-    [(and (empty? remain-mchs)
-          (empty? remain-eles)) #t]
-    [(empty? remain-mchs) #f] ; TODO maybe use continuation to back out
+    [(empty? remain-mchs) #f]
     [(only1? (first remain-mchs))
-     (error)]
+     (define nxt (handle-next (rest remain-mchs)))
+     (wcnode (first remain-mchs) (if nxt (cons 1 nxt) '()))]
     [else
      (match-define (cons low high) (get-consume-range (first remain-mchs)))
      (define low+ (max 0 (or low 0)))
@@ -27,10 +26,11 @@
      (define finished-children
        (for/fold ([children '()])
                  ([me-consume (range (sub1 high+) low+ -1)])
-         (define-values (_ after) (split-at remain-eles me-consume))
-         (define nxt (handle-next (rest remain-mchs) after))
+         (define nxt (handle-next (rest remain-mchs)))
          (if (boolean? nxt)
              children
              (cons (cons me-consume nxt) children))))
      (and (not (empty? finished-children))
           (wcnode (first remain-mchs) finished-children))]))
+
+#;(define (as-match ))
